1b70ad71c8272d43288b85ee3e51c17802f7b3a6
Oleksandr Shmyrko <oleksandr.shmyrko@gmail.com>
2026-05-18 01:31:56 +0300

    Add own kagent and other improvements

--- bootstrap/flux.tf
+++ bootstrap/flux.tf
@@ -1,3 +1,11 @@
+data "external" "github_user" {
+  program = ["bash", "-c", "git remote get-url origin | awk -F'[:/]' '{print $2}' | jq -R '{username: .}'"]
+}
+
+locals {
+  oci_registry = coalesce(var.oci_registry, "oci://ghcr.io/${data.external.github_user.result.username}/abox")
+}
+
 # ==========================================
 # Bootstrap Flux Operator
 # ==========================================
@@ -45,7 +53,7 @@ resource "kubectl_manifest" "rsip" {
         fluxcd.controlplane.io/reconcileEvery: 5m
     spec:
       type: OCIArtifactTag
-      url: ${var.oci_registry}/releases
+      url: ${local.oci_registry}/releases
       filter:
         includeTag: "^\\d+\\.\\d+\\.\\d+$"
         limit: 1
@@ -78,7 +86,7 @@ resource "kubectl_manifest" "rset" {
           namespace: flux-system
         spec:
           interval: 2m
-          url: ${var.oci_registry}/releases
+          url: ${local.oci_registry}/releases
           ref:
             tag: "<< inputs.tag >>"
       - apiVersion: kustomize.toolkit.fluxcd.io/v1
